4647902769a71efe616d54436861411ef112015b
benchokz <aruezechukwuebuka@gmail.com>
2023-04-07 16:54:44 +0100

    first commit

---
+++ server/Server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const cors = require("cors");
+
+const routes = require("./routes/ToDoRoute");
+
+const app = express();
+const PORT = process.env.PORT | 5000;
+
+app.use(express.json());
+app.use(cors());
+const uri = "mongodb://127.0.0.1:27017/myapp";
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Mongodb Connected..."))
+    .catch((err) => console.error(err));
+
+// Routes
+app.use(routes);
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
